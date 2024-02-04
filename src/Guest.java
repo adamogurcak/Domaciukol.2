8f3298bf9d4694d97afe5c4e55450d19a39ec3e3
adamogurcak <137228837+adamogurcak@users.noreply.github.com>
2024-02-04 13:11:45 +0100

    zadani cislo 2, ještě dodělat rezervace (moc jsem nepochopil)

--- src/Guest.java
+++ src/Guest.java
@@ -1,2 +1,65 @@
-package PACKAGE_NAME;public class Guest {
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Guest {
+
+
+
+
+    private String nameAndSurname;
+    private LocalDate birthdate;
+    private BigDecimal withBalconyAndSeaView;
+
+
+    private int roomNo;
+
+    public boolean canBuy(BigDecimal pricePerNight, int withBalconyAndSeaView)   {
+
+        BigDecimal totalPrice = pricePerNight.multiply(BigDecimal.valueOf(withBalconyAndSeaView));
+        return withBalconyAndSeaView >= 0;
+    }
+    public String getNameAndSurname() {
+        return nameAndSurname;
+    }
+
+    public Guest(String nameAndSurname, LocalDate birthdate) {
+        this(nameAndSurname, birthdate, BigDecimal.ZERO,1);;
+    }
+    public void setNameAndSurname(String nameAndSurname) {
+        this.nameAndSurname = nameAndSurname;
+    }
+
+    public LocalDate getBirthdate() {
+        return birthdate;
+    }
+
+    public void setBirthdate(LocalDate birthdate) {
+        this.birthdate = birthdate;
+    }
+
+    public int getRoomNo() {
+        return roomNo;
+    }
+
+    public BigDecimal getWithBalconyAndSeaView() {
+        return withBalconyAndSeaView;
+    }
+
+    public void setWithBalconyAndSeaView(BigDecimal withBalconyAndSeaView) {
+        this.withBalconyAndSeaView = withBalconyAndSeaView;
+    }
+
+    public void setRoomNo(int roomNo) {
+        this.roomNo = roomNo;
+
+
+    }
+
+    public Guest(String nameAndSurname, LocalDate birthdate, BigDecimal withBalconyAndSeaView, int roomNo) {
+        this.nameAndSurname = nameAndSurname;
+        this.birthdate = birthdate;
+        this.withBalconyAndSeaView = withBalconyAndSeaView;
+        this.roomNo = roomNo;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,77 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Guest guest1 = new Guest();
+        Guest guest2 = new Guest();
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        guest1.setNameAndSurname("Adéla Malíková");
+        LocalDate.of(1993, 3, 13),
+                BigDecimal.ZERO, 1;
+
+
+        guest2.setNameAndSurname("Jana Dvořáčka");
+        LocalDate.of(1995, 5, 5),
+        BigDecimal.ZERO, 2;
+
+        List<Guest> guestList = new ArrayList<Guest>();
+        guestList.add(guest1);
+        guestList.add(guest2);
+        System.out.println("Počet hostů: " + guestList);
+        System.out.println(guest1.getNameAndSurname());
+        for (int i = 1 <= 2; i++) {
+
+            guestList.add(new Guest("Host"+i, LocalDate.of(1990, 1, i)));
         }
+        System.out.println(guest2.getNameAndSurname());
+
+        System.out.println(guest1.canBuy(BigDecimal.TWO, 1));
+
+         guest1.setWithBalconyAndSeaView(BigDecimal.valueOf(1));
+        System.out.println("Jméno prvního hosta" +guestList.get(1).getNameAndSurname());
+        System.out.println("Jméno druhého hosta" +guestList.get(2).getNameAndSurname());
+
+
+
+
     }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
